0ff363bbee1226f8fd30f1191b1818052d49b03c
Laurence Hammond <laurencehammond@Laurences-MacBook-Pro.local>
2015-01-24 21:34:11 +0000

    First commit

---
+++ src/main/java/com/balourdos/BalourdosActivity.java
@@ -0,0 +1,23 @@
+package com.balourdos;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class BalourdosActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.balourdos_layout);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText("Hello world!");
+    }
+
+}
+
