831fb7fb5e0a759ef8a727841bf22055b99e5bcd
Konstantinos Daskalopoulos <k.t.daskalopoulos@gmail.com>
2015-03-29 22:21:08 +0300

    changing android manifest for google maps

---
+++ src/main/java/com/balourdos/Models/GoogleMapsConnection.java
@@ -0,0 +1,20 @@
+package com.balourdos.Models;
+import com.google.android.maps.*;
+
+
+/**
+ * Created by kostas on 29-Mar-15.
+ */
+public class GoogleMapsConnection {
+
+    private static final GoogleMapsConnection CONNECTION = new GoogleMapsConnection();
+
+    private GoogleMapsConnection(){
+
+    }
+
+    public GoogleMapsConnection getInstance()
+    {
+        return CONNECTION;
+    }
+}
