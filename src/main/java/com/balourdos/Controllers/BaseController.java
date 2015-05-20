bc5721a284366d2a24e49527580ecb10dc4abb0d
laurencehammond <laurence.hammond@bbc.co.uk>
2015-05-20 22:09:55 +0100

    removing import

--- src/main/java/com/balourdos/Controllers/BaseController.java
+++ src/main/java/com/balourdos/Controllers/BaseController.java
@@ -1,7 +1,6 @@
 package com.balourdos.Controllers;
 
 import com.balourdos.BalourdosApplication;
-import com.balourdos.Models.GPSTracker;
 import com.balourdos.Models.GoogleLocation;
 
 public class BaseController {
@@ -10,7 +9,7 @@ public class BaseController {
     public BaseController() {
         this.fusedLocation = new GoogleLocation();
     }
-    
+
     public void getFusedCurrentLocation()
     {
         System.out.println("fused location" + this.fusedLocation.getLastLocation());
