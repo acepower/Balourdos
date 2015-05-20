fced4025c2e9454a2ed29891a9534f2592a6b6aa
laurencehammond <laurence.hammond@bbc.co.uk>
2015-05-20 22:08:49 +0100

    refactoring

--- src/main/java/com/balourdos/Controllers/BaseController.java
+++ src/main/java/com/balourdos/Controllers/BaseController.java
@@ -5,22 +5,12 @@ import com.balourdos.Models.GPSTracker;
 import com.balourdos.Models.GoogleLocation;
 
 public class BaseController {
-    private GPSTracker gps;
     private GoogleLocation fusedLocation;
 
     public BaseController() {
-        this.gps = new GPSTracker(BalourdosApplication.getContext());
         this.fusedLocation = new GoogleLocation();
     }
-
-    public void getCurrentLocation() {
-        System.out.println(this.gps.canGetLocation());
-
-        if (this.gps.canGetLocation()) {
-            System.out.println(this.gps.getLatitude());
-            System.out.println(this.gps.getLongitude());
-        }
-    }
+    
     public void getFusedCurrentLocation()
     {
         System.out.println(this.fusedLocation.getLastLocation());
