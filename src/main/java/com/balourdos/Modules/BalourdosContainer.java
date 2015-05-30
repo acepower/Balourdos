30b287c1788def5118053c7b5ffb3fc671cc0111
laurencehammond <laurence.hammond@bbc.co.uk>
2015-05-30 18:50:49 +0100

    refactoring container

--- src/main/java/com/balourdos/Modules/BalourdosContainer.java
+++ src/main/java/com/balourdos/Modules/BalourdosContainer.java
@@ -3,11 +3,7 @@ package com.balourdos.Modules;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public final class BalourdosContainer {
-    private static GoogleComponent googleComponent;
-
-    public static void BalourdosContainer() {
-        googleComponent = DaggerGoogleComponent.builder().googleModule(new GoogleModule()).build();
-    }
+    private static GoogleComponent googleComponent = DaggerGoogleComponent.builder().googleModule(new GoogleModule()).build();
 
     public static GoogleApiClient getGoogleClient() {
         return googleComponent.provideGoogleApiClient();
